00a5d54c87e26957c15ddc6897c2b2e5a6fc33fd
yogeshbaral <barayoge@isu.edu>
2022-03-20 19:59:24 -0600

    Creating the joke service

---
+++ src/main/java/guru/springframework/spring5jokesappv2/service/JokeService.java
@@ -0,0 +1,5 @@
+package guru.springframework.spring5jokesappv2.service;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/service/JokeServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5jokesappv2.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+
+
+    @Override
+    public String getJoke() {
+        return null;
+    }
+}
