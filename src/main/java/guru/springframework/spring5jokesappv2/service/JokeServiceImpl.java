fe6b97318e5f5dc102125df05c37ee1c391c288f
yogeshbaral <barayoge@isu.edu>
2022-03-20 20:13:04 -0600

    Added controller, view and the implementation class

--- src/main/java/guru/springframework/spring5jokesappv2/service/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/service/JokeServiceImpl.java
@@ -16,6 +16,6 @@ public class JokeServiceImpl implements JokeService {
 
     @Override
     public String getJoke() {
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
